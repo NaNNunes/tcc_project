9fc640d9f58d3251ef0bd623e77657c24e9255d3
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2025-05-04 00:06:38 -0300

    Acrescentando os campos nos componentes de cadastro-demanda

--- tcc_projeto/src/pages/criar_demanda/Criar_demanda.jsx
+++ tcc_projeto/src/pages/criar_demanda/Criar_demanda.jsx
@@ -1,9 +1,13 @@
 import CategoriaDispositivo from '../../componentes/cadastro-demanda/CategoriaDispositivo/CategoriaDispositivo.jsx';
+import InformacoesDispositivo from '../../componentes/cadastro-demanda/InformacoesDispositivo/InformacoesDispositivo.jsx';
+import Contexto from '../../componentes/cadastro-demanda/Contexto/Contexto.jsx'
 
 const Criar_demanda = () => {
   return (
     <>
         <CategoriaDispositivo />
+        <InformacoesDispositivo />
+        <Contexto />
     </>
   )
 }
