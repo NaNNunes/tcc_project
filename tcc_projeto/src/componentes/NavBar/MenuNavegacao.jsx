89b3e85a3d73d2ab333c7f7ec170261410ad9134
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2025-05-21 16:23:20 -0300

    Mudando o menu para cima

--- tcc_projeto/src/componentes/NavBar/MenuNavegacao.jsx
+++ tcc_projeto/src/componentes/NavBar/MenuNavegacao.jsx
@@ -5,37 +5,53 @@ import {Container, Nav, Navbar, Image} from "react-bootstrap";
 import styles from "./MenuNavegacao.module.css"
 import { useState } from 'react';
 
+// Importação de icons
+import { TiArrowSortedDown } from "react-icons/ti";
+
 const MenuNavegacao = () => {
     const [hovered, setHovered] = useState(false); // useState do hover para Navbar.
 
   return (
     <div>
-        <Navbar className={styles.navbar} collapseOnSelect expand="lg" 
-            onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} // Função para quando o mouse entrar no Navbar mudar para true e quando sair mudar para false.
+        {/* <Navbar className={styles.navbar} collapseOnSelect expand="lg" 
+            onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}
         >
-            {/* Container com o icone ou logo da marca baseado no hover no Navbar. */}
             <Container className={styles.container}>
                 <Navbar.Brand href="/login" className={styles.brand}>
-                    {/* Quando tiver o hover muda de icone para logo. */}
                     {hovered 
                         ? <Image style={{minWidth: "276px", paddingRight: "15px", paddingLeft: "15px"}} src="/logos/connectfix_logo.svg" fluid />
                         : <Image style={{width: "75px", position: "relative", top: "-1px", left: "0px"}} src="/logos/connectfix_icon.svg" fluid />
                     }
                 </Navbar.Brand>
             
-                <Navbar.Collapse className={styles.navegacao} id="responsive-navbar-nav">
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className={styles.navegacao}>
-                        <hr className={styles.separar}/>
-                        {/* Mostra somente o icone quando estiver sem hover. Quando estiver com hover mostra o icone e o texto . */}
                         <Nav.Link className={styles.meuLink} href='/criar-Demanda'>
                             <Image className={styles.icone} src='../../../public/icons/Icon_pedido.svg' />
                             {hovered && <span className={styles.texto}>Cadastro pedido</span>}
                         </Nav.Link>
 
-                        <Nav.Link className={styles.meuLink} href='/criar-Demanda'>
-                            <Image className={styles.icone} src='../../../public/icons/Icon_consultar.svg'></Image>
-                            {hovered && <span className={styles.texto}>Consultar pedidos</span>}
-                        </Nav.Link>
+                        <NavDropdown title="Demandas" className={styles.dropdown} style={{color: '#ffffff'}}>
+                            <NavDropdown.Item menuVariant="danger" href='/criar-Demanda'>Cadastro pedido</NavDropdown.Item>
+                            <NavDropdown.Item>Consultar pedidos</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar> */}
+
+        <Navbar className={styles.navbar}>
+            <Container>
+                <Navbar.Brand href='/login'>
+                    <Image src="/logos/connectfix_logo.svg"></Image>
+                </Navbar.Brand>
+
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown title={<span className={styles.dropdown}>Demandas <TiArrowSortedDown color='white'/></span> } id="dropdown">
+                            <NavDropdown.Item href='/criar-Demanda'>Cadastro pedido</NavDropdown.Item>
+                            <NavDropdown.Item>Consultar pedidos</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
