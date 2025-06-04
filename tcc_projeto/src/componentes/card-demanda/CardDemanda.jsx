922bcf46ed5fe52bb5c9f8d16ca80677133c46f6
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2025-06-04 13:48:05 -0300

    Criação do componente de CardDemanda e pagina ProcurarDemanda, colocando a rota no navbar para procurar demanda

---
+++ tcc_projeto/src/componentes/card-demanda/CardDemanda.jsx
@@ -0,0 +1,17 @@
+// Importação do react-bootstrap
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+
+const CardDemanda = () => {
+  return (
+    <div>
+        <Container>
+            <Card>
+                <Card.Img />
+            </Card>
+        </Container>
+    </div>
+  )
+}
+
+export default CardDemanda
---
+++ tcc_projeto/src/pages/procurar-demandas/ProcurarDemandas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardDemanda from '../../componentes/card-demanda/CardDemanda'
+
+const ProcurarDemandas = () => {
+  return (
+    <>
+        <CardDemanda />
+    </>
+  )
+}
+
+export default ProcurarDemandas
